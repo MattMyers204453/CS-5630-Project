5c6e7552cebf1c0f8feba800babe668cb746e8f6
MattMyers204453 <mattbmyers@gmail.com>
2023-11-03 18:31:42 -0600

    change y-axis

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import { getData } from './data.js';
 
 let data = [];
 let first20 = [];
+let yAxisToShow = "subscribers";
 const HORIZONTAL_MARGIN = 100;
 const VERTICAL_MARGIN = 200;
 const CHART_WIDTH = 1200;
@@ -21,11 +22,10 @@ async function main() {
 function setup() {
     let svg = d3.select("#barchart-div")
         .append("svg")
-        .attr("width", CHART_WIDTH + HORIZONTAL_MARGIN*2)
-        .attr("height", CHART_HEIGHT + VERTICAL_MARGIN*2)
+        .attr("width", CHART_WIDTH + HORIZONTAL_MARGIN * 2)
+        .attr("height", CHART_HEIGHT + VERTICAL_MARGIN * 2)
         .append("g")
-            .attr("transform", "translate(" + HORIZONTAL_MARGIN + "," + VERTICAL_MARGIN + ")");
-
+        .attr("transform", "translate(" + HORIZONTAL_MARGIN + "," + VERTICAL_MARGIN + ")");
 
     // This is for individual channels
     let groups = d3.map(first20, (row) => {
@@ -44,7 +44,7 @@ function setup() {
     // Colors for the bars, change/add to these
     var color = d3.scaleOrdinal()
         .domain(subgroups)
-        .range(['#e41a1c','#377eb8', "#FFFF00"])
+        .range(['#e41a1c', '#377eb8', "#FFFF00"])
 
     let xSubGroup = d3.scaleBand()
         .domain(subgroups)
@@ -59,7 +59,7 @@ function setup() {
         .range([CHART_HEIGHT, 0]);
 
     svg.append("g")
-        .classed("current-y-scale", true)
+        .attr("id", "y-axis-subscribers")
         .call(d3.axisLeft(yScaleSubscribers));
 
     // Y-Axis for video views
@@ -70,7 +70,8 @@ function setup() {
         .range([CHART_HEIGHT, 0]);
 
     svg.append("g")
-        .classed("current-y-scale", false)
+        .classed("hidden", true)
+        .attr("id", "y-axis-views")
         .call(d3.axisLeft(yScaleVideoViews));
 
     // Y-Axis for number of uploads
@@ -79,7 +80,12 @@ function setup() {
     let yScaleUploads = d3.scaleLinear()
         .domain([0, maxYUploads])
         .range([CHART_HEIGHT, 0]);
-    
+
+    svg.append("g")
+        .classed("hidden", true)
+        .attr("id", "y-axis-uploads")
+        .call(d3.axisLeft(yScaleUploads));
+
 
     svg.append("g")
         .classed("x-scale", true)
@@ -99,37 +105,66 @@ function setup() {
         .data(first20)
         .enter()
         .append("g")
-            .attr("transform", (d) => {
-                //console.log(d);
-                return "translate(" + xScale(d.Youtuber) + ",0)";
-            })
-            .selectAll("rect")
-            .data(function(d) { return subgroups.map(function(key) { return {key: key, value: d[key]}; }); })
-            .enter().append("rect")
-              .attr("x", (d) => {
-                return xSubGroup(d.key); 
-            })
-                // This is where we scale the individual bars based on their name
-              .attr("y", (d) => { 
-                console.log(d);
-                if (d.key === "subscribers")
-                    return yScaleSubscribers(d.value);
-                else if (d.key === "video views")
-                    return yScaleVideoViews(d.value);
-                else if (d.key === "uploads")
-                    return yScaleUploads(d.value);
-            })
-              .attr("width", xSubGroup.bandwidth())
-              .attr("height", function(d) {
-                if (d.key === "subscribers") {
-                    return CHART_HEIGHT - yScaleSubscribers(d.value); 
-                }
-                else if (d.key === "video views")
-                    return CHART_HEIGHT - yScaleVideoViews(d.value);
-                else if (d.key === "uploads")
-                    return CHART_HEIGHT - yScaleUploads(d.value);
-                })
-              .attr("fill", function(d) { return color(d.key); });
+        .attr("transform", (d) => {
+            //console.log(d);
+            return "translate(" + xScale(d.Youtuber) + ",0)";
+        })
+        .selectAll("rect")
+        .data(function (d) { return subgroups.map(function (key) { return { key: key, value: d[key] }; }); })
+        .enter().append("rect")
+        .attr("x", (d) => {
+            return xSubGroup(d.key);
+        })
+        // This is where we scale the individual bars based on their name
+        .attr("y", (d) => {
+            console.log(d);
+            if (d.key === "subscribers")
+                return yScaleSubscribers(d.value);
+            else if (d.key === "video views")
+                return yScaleVideoViews(d.value);
+            else if (d.key === "uploads")
+                return yScaleUploads(d.value);
+        })
+        .attr("width", xSubGroup.bandwidth())
+        .attr("height", function (d) {
+            if (d.key === "subscribers") {
+                return CHART_HEIGHT - yScaleSubscribers(d.value);
+            }
+            else if (d.key === "video views")
+                return CHART_HEIGHT - yScaleVideoViews(d.value);
+            else if (d.key === "uploads")
+                return CHART_HEIGHT - yScaleUploads(d.value);
+        })
+        .attr("fill", function (d) { return color(d.key); })
+        .on("click", function (event, d) {
+            console.log("Clicked bar data:", d);
+            let newYAxisValue = (d.key === 'video views') ? 'views' : d.key;
+            console.log(newYAxisValue);
+            switchYAxis(newYAxisValue);
+        });
+}
+
+function switchYAxis(newYAxisValue) {
+    switch (newYAxisValue) {
+        case 'subscribers':
+            d3.select("#y-axis-subscribers").classed("hidden", false);
+            d3.select("#y-axis-views").classed("hidden", true);
+            d3.select("#y-axis-uploads").classed("hidden", true);
+            break;
+        case 'views':
+            d3.select("#y-axis-views").classed("hidden", false);
+            d3.select("#y-axis-subscribers").classed("hidden", true);
+            d3.select("#y-axis-uploads").classed("hidden", true);
+            break;
+        case 'uploads':
+            d3.select("#y-axis-uploads").classed("hidden", false);
+            d3.select("#y-axis-views").classed("hidden", true);
+            d3.select("#y-axis-subscribers").classed("hidden", true);
+            break;
+        default:
+            console.log("yAxis value not recognized");
+            return;
+    }
 }
 
 
